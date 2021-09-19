ea55f6641c179c87d984042b72d6da1ef77ed04b
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-19 11:06:37 +0300

    added flash and redirect from flas

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -19,3 +19,9 @@ def index():
         }
     ]
     return render_template('index.html', title= 'Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)    
+    
