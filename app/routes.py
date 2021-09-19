1b3a6bfb1f61770472b8541f54f85d2a44d598a1
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-19 09:38:31 +0300

    added the wft

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,19 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    """
-    a function that defines the route index
-    """
-    return "<h1> hello world </h1>"
+    user = {'username': 'Aysher'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title= 'Home', user=user, posts=posts)
