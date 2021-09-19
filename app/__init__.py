e500dac20a63d2ee0c4cdb86f8723449a2ec00fb
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-19 16:05:10 +0300

    wrote a fuction for login and logout

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_login.utils import login_user
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+login = LoginManager(app)
+login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
