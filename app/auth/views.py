b27273382fbf8334f038629c4e90426ecd46704f
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-23 13:26:58 +0300

    added a fuction to call the tepmlates

--- app/auth/views.py
+++ app/auth/views.py
@@ -17,7 +17,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "Pitch Perfect login"
+    title = "Acing Pitching login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -28,7 +28,7 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to Pitch Perfect","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Acing Pitches","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
