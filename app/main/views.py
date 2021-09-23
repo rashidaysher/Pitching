42db65e39efc73eb714fa359fcd6981d057fcccf
rashidaysher <aisha.ahmed@student.moringaschool.com>
2021-09-23 14:05:07 +0300

    deploying to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -4,7 +4,7 @@ from ..models import User,Pitch,Comment
 from .. import db,photos
 from .forms import UpdateProfile,PitchForm,CommentForm
 from flask_login import login_required,current_user
-import datetime
+
 
 # Views
 @main.route('/')
